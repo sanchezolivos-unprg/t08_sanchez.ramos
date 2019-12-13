a8de928bc55363d15962f4f58ca8133c98e5d5a9
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-13 03:14:25 -0500

    se agrego la longitud 3 en python file 2_longitud

--- sanchez/2_longitud.py
+++ sanchez/2_longitud.py
@@ -11,3 +11,10 @@ print("longitud = ", len(msg1)) # imprime la longitud de la cadena msg1
 msg2="DONDE VIVES"
 
 print("longitud = ", len(msg2)) # imprime la longitud de la cadena mg2
+
+# LONGITUD 3:
+#        0         1         2         3         4         5
+#        0123456789012345678901234567890123456789012345678901234
+cadena1="ESTIPHEN HAWKING DIJO, MIENTRAS HAYA VIDA HAY ESPERANZA"
+
+print("la cadena 1 tiene", len(cadena1), "elementos") # imprime la longitud de la cadena 1
