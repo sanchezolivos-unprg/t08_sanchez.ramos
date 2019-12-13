13c85743bbfce5ea0ab95c68f3720d4f8079c415
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-13 03:32:38 -0500

    se agrego el cortado de cadenas 1 en el python file 5_cortado.py

---
+++ sanchez/5_cortado.py
@@ -0,0 +1,13 @@
+# CORTADO DE CADENAS 1:
+#     0         1         2         3         4         5         6
+#     0123456789012345678901234567890123456789012345678901234567890123456
+msg1="LA UNIVERSIDAD NACIONAL PEDRO RUIZ GALLO SE ENCUENTRA EN LAMBAYEQUE"
+
+# mostrar UNIVERSIDAD
+print(msg1[3:14])
+
+# mostrar OLLA
+print(msg1[39:35:-1])
+
+# mostrar PEDRO
+print(msg1[24:29])
