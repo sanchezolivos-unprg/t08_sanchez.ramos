132609630367d15b1a1a5a3cfb09dee53aa25c2d
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-13 03:22:38 -0500

    se agrego el comparacion del 1 al 15 en el python file 3_comparacion.py

---
+++ sanchez/3_comparacion.py
@@ -0,0 +1,154 @@
+# COMPARACION 1:
+
+pregunta="EL LEON ES UN CARNIVORO?"
+print(pregunta)
+rpta=input("ingrese la respuesta: ")
+
+# comparacion
+if (rpta!="si"):
+    print("respuesta incorrecta")
+else:
+    print("respuesta correcta")
+
+# COMPARACION 2:
+
+# comparacion d si a es igual a d
+print("a" == "d")
+
+# COMPARACION 3:
+# comparacion de una suma
+pta="cuanto es 104 + 3056?"
+print(pta)
+result=input("ingrese el resultado: ")
+
+if (result == "3160"):
+    print("el resulatado es verdadero")
+else:
+    print("el resultado es falso")
+
+# COMPARACION 4:
+
+pta4="los mosqueteros fueron 3?"
+print(pta4)
+result4=input("ingrese (si/no): ")
+
+if (result4 == "si"):
+    print("el resulatado es verdadero")
+else:
+    print("el resultado es falso")
+
+# COMPARACION 5:
+# comparacion de si el numero 45 es mayor que 50
+pta5="45 > 50?"
+print(pta5)
+result5=input("ingrese (si/no): ")
+
+if (result5 == "si"):
+    print("la respuesta es falso")
+else:
+    print("la respuesta es verdadero")
+
+# COMPARACION 6:
+# comparacion de si 50 es igual a 50
+pta6="50 = 50?"
+print(pta6)
+result6=input("ingrese (v/f): ")
+
+if (result6 != "f"):
+    print("la respuesta es correcta")
+else:
+    print("la respuesta es incorrecta")
+
+# COMPARACION 7:
+# comparacion de una resta
+pta7="180 - 54 = 126?"
+print(pta7)
+result7=input("ingrese (si/no): ")
+
+if (result7 == "no"):
+    print("el resultado es falso")
+else:
+    print("el resultado es verdadero")
+
+# COMPARACION 8:
+# comparacion de la multiplicacion de 200 y 5
+pta8="200 x 5 = 1000?"
+print(pta8)
+result8=input("ingrese (v/f) :")
+
+if (result8 == "v"):
+    print("el resultado es correcto")
+else:
+    print("el resultado es incorrecto")
+
+# COMPARACION 9:
+# comparacion de la division de 802 y 2
+pta9="802 / 2 = 403?"
+print(pta9)
+result9=input("ingrese (si/no): ")
+
+if (result9 != "si"):
+    print("VERDADERO")
+else:
+    print("FASO")
+
+# COMPARACION 10:
+
+pta10="la capital de peru es lima?"
+print(pta10)
+result10=input("ingrese (si/no): ")
+
+if (result10 == "si"):
+    print("la respuesta es correcta")
+else:
+    print("la respuesta es incorrecta")
+
+# COMPARACION 11:
+
+pta11="cuantos son las estaciones del año?"
+print(pta11)
+result11=input("ingrese la respuesta: ")
+
+if (result11 == "4"):
+    print("la respuesta es verdadera")
+else:
+    print("la respueta es falsa")
+
+# COMPARACION 12:
+# comparacion de los lados de un cuadrado
+pta12="un cuadrdado cuantos lados tiene?"
+print(pta12)
+result12=input("ingrese la respuesta: ")
+
+if (result12 == "4"):
+    print("la respuesta es correcta")
+else:
+    print("la respuesta es incorrecta")
+
+# COMPARACION 13:
+# comparacion si el hexagono tiene 7 lados
+pta13="el hexagono tiene 7 lados"
+print(pta13)
+result13=input("ingrese si es (v/f): ")
+
+if (result13 =="f"):
+    print("la respuesta es correcta")
+else:
+    print("la respuesta es incorrecta")
+
+# COMPARACION 14:
+# comparacion entre las cadenas si es mayor
+cadena1="4567"
+cadena2="8795"
+
+print(cadena2 > cadena1)
+
+# COMPARACION 15:
+# comparacion entre rusia y japon
+pais1="rusia"
+pais2="japon"
+
+calculo=(pais1>pais2)
+calculo=True
+
+print(pais1>pais2)
