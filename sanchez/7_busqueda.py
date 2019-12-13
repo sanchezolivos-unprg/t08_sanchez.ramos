ae8766d6b2e0f091f659932d616e971d25608dcf
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-13 03:42:53 -0500

    se agrego busqueda de cadena 2 en el python file 7_busqueda.py

--- sanchez/7_busqueda.py
+++ sanchez/7_busqueda.py
@@ -5,3 +5,11 @@ cadena1="LOS CUATRO FANTASTICOS"
 
 print(cadena1.find("A"))
 # FIN_BUSQUEDA
+
+# BUSQUEDA DE CADENA 2:
+#        0         1         2
+#        01234567890123456789012345
+cadena2="SIETE MAS OCHO ES QUINCE"
+
+print(cadena2.find("OCHO"))
+# FIN_BUSQUEDA
