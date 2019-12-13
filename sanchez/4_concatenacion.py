76c2f582612797c0f9942baeb474fed05ad5d839
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-13 03:25:58 -0500

    se agrego la concatenacion 2 en el python file 4_concatenacion.py

--- sanchez/4_concatenacion.py
+++ sanchez/4_concatenacion.py
@@ -6,3 +6,12 @@ prom_vida=14
 # agrupacion de cadenas
 mensaje1=("el " + animal + " es el rey de la selva, su peso es de " + str(mc_kg) + "kg y su promedio de vida es de " + str(prom_vida) + " años")
 print(mensaje1)
+
+# CONCATENACION 2:
+nombre="claudio"
+edad=15
+colegio="Pedro Abel Labathe Durand"
+
+# agrupacion de cadenas
+mensaje2=("el estudiante " + nombre + " tiene " + str(edad) + " años" + " y estudia en el colegio " + colegio)
+print(mensaje2)
