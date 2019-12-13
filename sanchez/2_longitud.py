1e9c50cec35c615216223c0bdf934babaa71c1ea
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-13 03:13:44 -0500

    se agrego la longitud 2 en python file 2_longitud

--- sanchez/2_longitud.py
+++ sanchez/2_longitud.py
@@ -4,3 +4,10 @@
 msg1="CUAL ES TU NOMBRE"
 
 print("longitud = ", len(msg1)) # imprime la longitud de la cadena msg1
+
+# LONGITUD 2:
+#     0         1
+#     01234567890
+msg2="DONDE VIVES"
+
+print("longitud = ", len(msg2)) # imprime la longitud de la cadena mg2
