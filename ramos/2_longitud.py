090d27aef156e7d5cd5ab83752c9178f6b25d854
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-13 04:06:44 -0500

    se agrego la continuacion de la operacion longitud desde el ejercicio 16 hasta 31 en el directorio ramos

---
+++ ramos/2_longitud.py
@@ -0,0 +1,82 @@
+# CONTINUACION DE LA OPERACION LOGITUD
+#MANIPULACION DE TEXTOS
+
+#manipulador de texto nro 16
+#       0123456789012345678
+cadena="HOY TE IRÁ MUY BIEN"
+print("la cadena",cadena,"tiene :",len(cadena),"elementos")
+
+#manipulador de texto nro 17
+#       01234567890123456789012
+cadena="SONRIE LA VIDA ES ÚNICA"
+print("la cadena",cadena,"tiene :",len(cadena),"elementos")
+
+#manipulador de texto nro 18
+#       01234567890123456789
+cadena="LA VIDA ES UN REGALO"
+print("la cadena",cadena,"tiene :",len(cadena),"elementos")
+
+#manipulador de texto nro 19
+#       0123456789012345678901
+cadena="INGENIERÍA ELECTRONICA"
+print("la cadena",cadena,"tiene :",len(cadena),"elementos")
+
+#manipulador de texto nro 20
+#       012345678901234567890
+cadena="INDEXACION DE CADENAS"
+print("la cadena",cadena,"tiene :",len(cadena),"elementos")
+
+#manipulador de texto nro 21
+#       0123456789012345678901234567890123456
+cadena="UNIVERSIDAD NACIONAL PEDRO RUIZ GALLO"
+print("la cadena",cadena,"tiene :",len(cadena),"elementos")
+
+#manipulador de texto nro 22
+#       0123456789012345678901
+cadena="CURSO DE PROGRMACION I"
+print("la cadena",cadena,"tiene :",len(cadena),"elementos")
+
+#manipulador de texto nro 23
+#       01234567890123456789012
+cadena="OPERACIONES CON CADENAS"
+print("la cadena",cadena,"tiene :",len(cadena),"elementos")
+
+#manipulador de texto nro 24
+cadena="ACUERDATE DE MI"
+print(cadena, len(cadena))
+
+#manipulador de texto nro 25
+#          012345678901234
+print(len("hola mundo \r\n"))    #en este caso la cadena tiene 13 elementos se excluyen los backslash
+
+#manipulador de texto nro 26
+cadena=" "
+print(len(cadena))               #tendria un elemento que es el espacio
+
+#manipulador de texto nro 27
+#       012345678901234567890123456789
+cadena="21313 ; ES EL NUMERO DE CUENTA"
+print("la cadena",cadena,"tiene :",len(cadena),"elementos")
+
+#manipulador de texto nro 28
+#       0123456789012345
+cadena="     HOLA MUNDO "
+print("la cadena",cadena,"tiene :",len(cadena),"elementos")
+
+#manipulador de texto nro 29
+#       012345678901234567890123456789
+cadena=" \\ es una barra invertida"
+print(cadena, len(cadena))#en este caso el backslash es contado como un elemento , cadena tiene 25 elemntos
+
+#manipulador de texto nro 30
+#                 10        20        30        40
+#       01234567890123456789012345678901234567890
+cadena="OPERACIONES CON CADENAS \"ES MUY SIMPLE\""
+print("la cadena",cadena,"tiene :",len(cadena),"elementos")#los elementos \" son contadas como un elemento
+
+#manipulador de texto nro 31
+#              10        20        30
+#    0123456789012345678901234567890123456789
+str="PYTHON ES UN \"LENGUAJE DE PROGRMACION\"" #los elemntos \" son contadas como un elemento
+print(len(str))
+
