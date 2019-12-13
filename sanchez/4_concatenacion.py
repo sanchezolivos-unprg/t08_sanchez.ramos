6679b266258e5c2987df14d6c7491855380fb6c1
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-13 03:29:50 -0500

    se agrego la concatenacion 7, 8, 9, 10, 11, 12, 13, 14 y 15 en el python file 4_concatenacion.py

--- sanchez/4_concatenacion.py
+++ sanchez/4_concatenacion.py
@@ -53,3 +53,81 @@ esc="FIA"
 # agrupacion de cadenas
 mensaje6=("el estudiante " + est + " de " + str(edad) + " años" + " pertenece en el ciclo " + str(ciclo) + " en la escuela de " + esc)
 print(mensaje6)
+
+# CONCATENACION 7:
+est="DIANA"
+edad=25
+ciclo=7
+carr="DERECHO"
+
+# agrupacion de cadenas
+mensaje7=("la estudiante " + est + " de " + str(edad) + " años" + " pertenece en el ciclo " + str(ciclo) + " y estudia la carrera de " + carr)
+print(mensaje7)
+
+# CONCATENACION 8:
+est="BRUNO"
+edad=30
+ciclo=9
+esc="INGENIERIA CIVIL"
+
+# agrupacion de cadenas
+mensaje8=("el estudiante " + est + " de " + str(edad) + " años" + " pertenece en el ciclo " + str(ciclo) + " y estudia la carrera de " + esc)
+print(mensaje8)
+
+# CONCATENACION 9:
+est="GABRIELA"
+edad=19
+ciclo=3
+esc="ENFERMERIA"
+
+# agrupacion de cadenas
+mensaje9=("la estudiante " + est + " de " + str(edad) + " años" + " pertenece en el ciclo " + str(ciclo) + " y estudia la carrera de " + esc)
+print(mensaje9)
+
+# CONCATENACION 10:
+est="PATRICIO"
+edad=32
+ciclo=10
+esc="BIOLOGIA"
+
+# agrupacion de cadenas
+mensaje10=("el estudiante " + est + " de " + str(edad) + " años" + " pertenece en el ciclo " + str(ciclo) + " y estudia la carrera de " + esc)
+print(mensaje10)
+
+# CONCATENACION 11:
+cient="STEPHEN HAWKING"
+pais="INGLATERRA"
+ofic="fisico"
+
+dato=("el cientifico " + cient + " nacio en " + pais + " y era un gran " + ofic)
+print(dato)
+
+# CONCATENACION 12:
+nombre="BILL GATES"
+edad=64
+nacion="ESTADOS UNIDOS"
+
+dato1=("el empresario informatico " + nombre + " de " + str(edad) + " años " + "nacio en " + nacion + " y es cofundador de software microsoft")
+print(dato1)
+
+# CONCATENACION 13:
+oper="25 + 28"
+numeros="naturales"
+
+dato2=("la suma de " + "los numeros " + numeros + " " + oper + " = " + "53")
+print(dato2)
+
+# CONCATENACION 14:
+oper="15.8 + 18.6"
+numeros="decimales"
+
+dato3=("la suma de la operacion " + "de numeros " + numeros + " " + oper + " = " + "34.4")
+print(dato3)
+
+# CONCATENACION 15:
+numero1=48
+numero2=6
+numero="naturales"
+
+dato4=("la multiplicacion " + "de los numeros " + numero + " " + str(numero1) + " x " + str(numero2) + " = " + "384")
+print(dato4)
