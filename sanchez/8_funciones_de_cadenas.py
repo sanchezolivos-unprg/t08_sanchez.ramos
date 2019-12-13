be734b20d75d88171cf7e61c33b76d3c49ab9f41
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-13 03:48:16 -0500

    se agrego funciones de manipulacion de cadena 1 en el python file 8_funciones_de_cadena.py

---
+++ sanchez/8_funciones_de_cadenas.py
@@ -0,0 +1,6 @@
+# FUNCIONES DE MANIPULACION DE CADENA 1:
+msg="EL FIN DEL MUNDO SE ACERCA"
+
+# Mostrar el nro de ocurrencias de la palabra FIN
+print("FIN",msg.count("FIN"))
+print("A ->", msg.count("A"))
