62f3a1833d997266b7123ad37058906edcf69413
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-13 03:40:05 -0500

    se agrego el iteracion del ejercicio 1 hasta el ejercicio 15 en el python file 6_iteracion.py

---
+++ sanchez/6_iteracion.py
@@ -0,0 +1,220 @@
+# ITERACION 1:
+mensaje="ASI ES LA VIDA"
+contador_a=0
+contador_e=0
+contador_i=0
+contador_o=0
+contador_u=0
+
+for letra in mensaje:
+    if (letra == 'A'):
+        contador_a+=1
+    if (letra == 'E'):
+        contador_e+=1
+    if (letra == 'I'):
+        contador_i+=1
+    if (letra == 'O'):
+        contador_o+=1
+    if (letra == 'U'):
+        contador_u+=1
+
+# Contar cuantas vocales existen en la cadena
+print("A: ",contador_a)
+print("E: ",contador_e)
+print("I: ",contador_i)
+print("O: ",contador_o)
+print("U: ",contador_u)
+# FIN_ITERACION
+print()
+
+# ITERACION 2:
+mensaje="PODRAN ROBARTE LAS IDEAS PERO NUNCA EL TALENTO"
+contador_a=0
+contador_e=0
+contador_i=0
+contador_o=0
+contador_u=0
+
+for letra in mensaje:
+    if (letra == 'A'):
+        contador_a+=1
+    if (letra == 'E'):
+        contador_e+=1
+    if (letra == 'I'):
+        contador_i+=1
+    if (letra == 'O'):
+        contador_o+=1
+    if (letra == 'U'):
+        contador_u+=1
+
+# Contar cuantas vocales existen en la cadena
+print("A: ",contador_a)
+print("E: ",contador_e)
+print("I: ",contador_i)
+print("O: ",contador_o)
+print("U: ",contador_u)
+# fin_iteracion
+print()
+
+# ITERACION 3:
+mensaje="ANALISIS MATEMATICO ES MI CURSO"
+contador_a=0
+contador_e=0
+contador_i=0
+contador_o=0
+contador_u=0
+
+for letra in mensaje:
+    if (letra == 'A'):
+        contador_a+=1
+    if (letra == 'E'):
+        contador_e+=1
+    if (letra == 'I'):
+        contador_i+=1
+    if (letra == 'O'):
+        contador_o+=1
+    if (letra == 'U'):
+        contador_u+=1
+
+# Contar cuantas vocales existen en la cadena
+print("A: ",contador_a)
+print("E: ",contador_e)
+print("I: ",contador_i)
+print("O: ",contador_o)
+print("U: ",contador_u)
+# fin_iteracion
+print()
+
+# ITERACION 4:
+mensaje="SUEÑA PERO NO TE DUERMAS"
+
+# mostrar el mensaje
+for i in mensaje:
+    print(end=i)
+# FIN_ITERACION
+print()
+
+# ITERACION 5:
+mensaje="HOLA MI NOMBRE ES LIONEL ANDERSON"
+
+# mostrar el mensaje
+for y in mensaje:
+    print(y)
+# fin_iteracion
+print()
+
+# ITERACION 6:
+carrera="INGENIERIA COMPUTACION E INFORMATICA"
+
+# mostrar la cadena
+for letra in carrera:
+    print(letra)
+
+# ITERACION 7:
+mensaje="SON LAS CINCO DE LA MAÑANA"
+
+# mostrar el mensaje
+for i in mensaje:
+    print(end=i)
+# FIN_ITERACION
+print()
+
+# ITERACION 8:
+mensaje="MAÑANA HAY CLASES?"
+
+# mostrar el mensaje
+for i in mensaje:
+    print(end=i)
+# FIN_ITERACION
+print()
+
+# ITERACION 9:
+mensaje="ESTAMOS LUNES"
+
+# mostrar el mensaje
+for i in mensaje:
+    print(end=i)
+# FIN_ITERACION
+print()
+
+# ITERACION 10
+mensaje="KOALA"
+
+# mostrar el mensaje
+for i in mensaje:
+    print(end=i)
+# FIN_ITERACION
+print()
+
+# ITERACION 11:
+mensaje="WALTER ES DOCTOR"
+
+# mostrar el mensaje
+for i in mensaje:
+    print(end=i)
+# FIN_ITERACION
+print()
+
+# ITERACION 12:
+nombre="SINDY CLAUDIA"
+
+# mostrar el nombre
+for letra in nombre:
+    print(letra)
+# fin_iteracion
+print()
+
+# ITERACION 12:
+apellidos="SANTISTEBAN RODRIGUEZ"
+
+# mostrar lo apellidos
+for letra in apellidos:
+    print(letra)
+# fin_iteracion
+print()
+
+# ITERACION 13:
+dicho="ARBOL QUE CRECE TORCIDO NO SE PUEDE ENDEREZAR"
+
+# mostrar el dicho
+for i in dicho:
+    print(end=i)
+# fin_iteracion
+print()
+
+# ITERACION 14:
+frase="LO QUE BIEN SE PIENSA, BIEN SE EXPRESA"
+
+# mostrar la frase
+for i in frase:
+    print(end=i)
+# fin_iteracion
+print()
+
+# ITERACION 15:
+mensaje="CUANDO HAY GANAS TODO SE PUEDE"
+contador_a=0
+contador_e=0
+contador_i=0
+contador_o=0
+contador_u=0
+
+for letra in mensaje:
+    if (letra == 'A'):
+        contador_a+=1
+    if (letra == 'E'):
+        contador_e+=1
+    if (letra == 'I'):
+        contador_i+=1
+    if (letra == 'O'):
+        contador_o+=1
+    if (letra == 'U'):
+        contador_u+=1
+
+# Contar cuantas vocales existen en la cadena
+print("A: ",contador_a)
+print("E: ",contador_e)
+print("I: ",contador_i)
+print("O: ",contador_o)
+print("U: ",contador_u)
+# FIN_ITERACION
