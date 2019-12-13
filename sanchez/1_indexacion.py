4806fe4806160fa9ca70f59cbae811d6e5c3b662
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-13 02:55:10 -0500

    se agrego la indexacion 1 en python file 1_indexacion

---
+++ sanchez/1_indexacion.py
@@ -0,0 +1,9 @@
+# INDEXACION 1:
+# una escuela de la universidad
+#    0         10        20        30
+#    012345678901234567890123456789012
+esc="ESCUELA DE INGENIERIA ELECTRONICA"
+
+# pais= esc[18] + esc[3] + esc[1] + esc[19] + esc[20]
+print(esc[18], esc[3], esc[1], esc[19], esc[20])
+
