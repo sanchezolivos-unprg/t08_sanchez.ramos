dbe32c5dcd25b91e490d049957ff6fdc4afef3e2
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-13 03:19:45 -0500

    se agrego la longitud 9, 10, 11, 12, 13, 14 y 15 en python file 2_longitud

--- sanchez/2_longitud.py
+++ sanchez/2_longitud.py
@@ -53,3 +53,52 @@ print("la longitud de la cadena 5: ", cadena5, "tiene", len(cadena5), "elementos
 cadena6="QUE NADA NI NADIE CONDICIONE TU ALEGRIA"
 
 print("la longitud de la cadena 6: ", cadena6, "tiene", len(cadena6), "elementos") # imprime la longitud de la cadena 6
+
+# LONGITUD 9:
+#        0         1         2         3         4         5
+#        012345678901234567890123456789012345678901234567890
+cadena7="TU PRESENCIA EN MI VIDA ES UNA GRAN FORTUNA PARA MI"
+
+print("la longitud de la cadena 7: ", cadena7, "tiene", len(cadena7), "elementos") # imprime la longitud de la cadena 7
+
+# LONGITUD 10:
+#        0         1         2         3
+#        0123456789012345678901234567890
+cadena8="BUSCANDO A CRISTO EN MI CORAZON"
+
+print("la longitud de la cadena 8: ", cadena8, "tiene", len(cadena8), "elementos") # imprime la longitud de la cadena 8
+
+# LONGITUD 11:
+#        0         1         2         3         4         5
+#        01234567890123456789012345678901234567890123456789012345678
+cadena9="TE ESPERO CUANDO MIREMOS AL CIELO DE NOCHE: TU ALLA, YO AQUI"
+
+print("la longitud de la cadena 9: ", cadena9, "tiene", len(cadena9), "elementos") # imprime la longitud de la cadena 9
+
+# LONGITUD 12:
+#         0                   1         2         3         4
+#         01234567890123456789012345678901234567890123456789012345
+cadena10="SI ME ABRAZAS, EL MUNDO SE PUEDE DETENER EN ESE INSTANTE"
+
+print("la longitud de la cadena 10: ", cadena10, "tiene", len(cadena10), "elementos") # imprime la longitud de la cadena 10
+
+# LONGITUD 13:
+#         0         1         2         3         4         5         6         7         8
+#         0123456789012345678901234567890123456789012345678901234567890123456789012345678901
+cadena11="CON UNA SONRISA ME ROBASTRE EL CORAZON, Y CON UN BESO VOLE A LO MAS ALTO DEL CIELO"
+
+print("la longitud de la cadena 11: ", cadena11, "tiene", len(cadena11), "elementos") # imprime la longitud de la cadena 11
+
+# LONGITUD 14:
+#         0         1         2         3         4         5         6         7
+#         0123456789012345678901234567890123456789012345678901234567890123456789012345678
+cadena12="LAS BUENAS ACCIONES VIENEN DE LOS BUENOS PENSAMIENTOS, Y ESTOS PROCEDEN DE DIOS"
+
+print("la longitud de la cadena 12: ", cadena12, "tiene", len(cadena12), "elementos") # imprime la longitud de la cadena 12
+
+# LONGITUD 15:
+#        0         1         2         3         4
+#        012345678901234567890123456789012345678901234567
+mensaje="EL CORAZON DE UNA MADRE ES LA ESCUELA DE UN NIÑO"
+
+print("la longitud de la cadena: ", mensaje, "tiene", len(mensaje), "elementos") # imprime la longitud de la cadena mensaje
