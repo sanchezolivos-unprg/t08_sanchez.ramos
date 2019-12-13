7c0500f396eccc74b5117c65d3402481e70ed756
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-13 03:45:28 -0500

    se agrego busqueda de cadena 5 hasta 15 en el python file 7_busqueda.py

--- sanchez/7_busqueda.py
+++ sanchez/7_busqueda.py
@@ -29,3 +29,91 @@ cadena4="NUEVE MAS CINCO ES IGUAL A CATORCE"
 
 print(cadena4.index("CINCO"))
 # FIN_BUSQUEDA
+
+# BUSQUEDA DE CADENA 5:
+#        0         1         2
+#        01234567890123456789012
+cadena5="RODRIGO NACIO EN ITALIA"
+
+print(cadena5.find("ITALIA"))
+# FIN_BUSQUEDA
+
+# BUSQUEDA DE CADENA 6:
+#        0         1
+#        012345678901
+cadena6="DANCE MONKEY"
+
+print(cadena6.find("MONKEY"))
+# FIN_BUSQUEDA
+
+# BUSQUEDA DE CADENA 7:
+#        0         1
+#        012345678901234
+cadena7="COLOR ESPERANZA"
+
+print(cadena7.index("ESPERANZA"))
+# FIN_BUSQUEDA
+
+# BUSQUEDA DE CADENA 8:
+#        0         1
+#        01234567890123456789
+cadena8="EL MUNDO ES PERFECTO"
+
+print(cadena8.find("MUNDO"))
+# FIN_BUSQUEDA
+
+# BUSQUEDA DE CADENA 9:
+#        0         1         2
+#        012345678901234567890123456789
+cadena9="EL CIELO TAN ALTO Y CRISTALINO"
+
+print(cadena9.index("CIELO"))
+# FIN_BUSQUEDA
+
+# BUSQUEDA DE CADENA 10:
+#         0         1         2
+#         012345678901234567890123456789
+cadena10="LA CAPITAL DE ESPAÑA ES MADRID"
+
+print(cadena10.find("ESPAÑA"))
+# FIN_BUSQUEDA
+
+# BUSQUEDA DE CADENA 11:
+#         0         1         2         3         4
+#         01234567890123456789012345678901234567890123
+cadena11="EL IDIOMA OFICIAL DE BARCELONA ES EL CATALAN"
+
+print(cadena11.index("BARCELONA"))
+# FIN_BUSQUEDA
+
+# BUSQUEDA DE CADENA 12:
+#         0         1         2         3
+#         012345678901234567890123456789012345678
+cadena12="RUSIA ES EL PAIS MAS GRANDE DEL PLANETA"
+
+print(cadena12.find("GRANDE"))
+# FIN_BUSQUEDA
+
+# BUSQUEDA DE CADENA 13:
+#         0         1
+#         01234567890123456789
+cadena13="LOS TRES MOSQUETEROS"
+
+print(cadena13.find("MOSQUETEROS"))
+# FIN_BUSQUEDA
+
+# BUSQUEDA DE CADENA 14:
+#         0         1         2
+#         0123456789012345678901234567890
+cadena14="DIEZ ENTRE CINCO ES IGUAL A DOS"
+
+print(cadena14.index("CINCO"))
+# FIN_BUSQUEDA
+
+# BUSQUEDA DE CADENA 15:
+#         0         1         2
+#         0123456789012345678901234
+cadena15="INGRESE A LA UNIVERSIDAD!"
+
+print(cadena15.index("UNIVERSIDAD!"))
+# FIN_BUSQUEDA
