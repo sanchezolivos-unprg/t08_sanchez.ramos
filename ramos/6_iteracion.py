d01dcdb7402dacd14664760b1f9fe5f044919b84
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-13 04:21:56 -0500

    se agrego la continuacion de la operacion iteracion desde 16 hasta el ejercicio 30 en el directorio ramos

---
+++ ramos/6_iteracion.py
@@ -0,0 +1,133 @@
+#OPERACION DE ITERACIÓN
+
+#manipulador de texto nro 16
+cadena=" python es un lenguaje de programación "
+#mostrar el mensaje :python es un lenguaje de programacion
+for i in cadena:
+    print(end=i)
+
+#manipulador de texto nro 17
+cad=" hola mundo"
+for y in cad:
+    print(y)
+
+#manipulador de texto nro 18
+cad1="PIENSA GLOBAL;ACTUA LOCAL"
+for j in cad1:
+    print(j)
+
+#manipulador de texto nro 19
+#mostrar la cantidad de la vocales E
+msg="EL LOGRO DE UN AMIGO SE FESTEJA COMO SI FUERA TUYO"
+contador_a=0
+for t in msg:
+    if(t=="A"):
+        contador_a+=1
+print("A:",contador_a)
+
+#manipulador de texto nro 20
+msg_1="EL LOGRO DE UN AMIGO, SE FESTEJA COMO SI FUERA TUYO"
+#mostra cuantas letras T existen el cadena
+contador_t=0
+for g in msg_1:
+    if(g=="T"):
+        contador_t+=1
+print("T:",contador_t)
+
+#manipulador de texto nro 21
+msg_2="NO HAY NADA QUE ENSEÑE MAS, QUE EQUIVOCARSE"
+#mostrar las letras N O E A Q
+contador_n=0
+contador_o=0
+contador_e=0
+contador_a=0
+contador_q=0
+
+for b in msg_2:
+    if (b=="N"):
+        contador_n+=1
+    if (b=="O"):
+        contador_o+=1
+    if (b=="E"):
+        contador_e+=1
+    if (b=="A"):
+        contador_a+=1
+    if (b=="Q"):
+        contador_q+=1
+
+print("N:",contador_n)
+print("O:",contador_o)
+print("E:",contador_e)
+print("A:",contador_a)
+print("Q:",contador_q)
+
+#manipulador de texto nro 22
+frase="QUE TENGAS UNN FELIZ DIA"
+#mostrar el mensaje :python es un lenguaje de programacion
+for i in cadena:
+    print(end=i)
+
+#manipulador de texto nro 23
+frase="QUE TENGAS UNN FELIZ DIA"
+#mostrar el mensaje :QUE TENGAS UNN FELIZ DIA
+for i in frase:
+    print(end=i)
+
+#manipulador de texto nro 24
+#mostrar la cantidad de la vocales I
+msg_5="EL LOGRO DE UN AMIGO SE FESTEJA COMO SI FUERA TUYO"
+contador_i=0
+for o in msg:
+    if(o=="I"):
+        contador_i+=1
+print("I:",contador_i)
+
+#manipulador de texto nro 25
+msg_8="NO HAY NADA QUE ENSEÑE MAS, QUE EQUIVOCARSE"
+#mostrar los numeros  3 2 7
+contador_N=0
+contador_O=0
+contador_E=0
+
+for P in msg_8:
+    if (P=="N"):
+        contador_N+=1
+    if (P=="O"):
+        contador_O+=1
+    if (P=="E"):
+        contador_E+=1
+
+print(contador_N,contador_O,contador_E)
+
+#manipulador de texto nro 26
+cad7="PIENSA GLOBAL;ACTUA LOCAL"
+for l in cad7:
+    print(l)
+
+#manipulador de texto nro 27
+frase="ALGUN DIA LLEGARA TU MOMENTO"
+#mostrar el mensaje :ALGUN DIA LLEGARA TU MOMENTO
+for u in frase:
+    print(end=u)
+
+#manipulador de texto nro 28
+#mostrar la cantidad de la vocales U
+frase_5="EL LOGRO DE UN AMIGO SE FESTEJA COMO SI FUERA TUYO"
+contador_u=0
+for t in msg:
+    if(t=="U"):
+        contador_u+=1
+print("U:",contador_u)
+
+#manipulador de texto nro 29
+frase3="QUE TENGAS UNN FELIZ DIA"
+#mostrar Del mensaje la letra F
+for f in frase3:
+    if (f=="F"):
+            print("F")
+
+#manipulador de texto nro 30
+
+numero_te="747518"
+for s in numero_te:
+    print("hola")
