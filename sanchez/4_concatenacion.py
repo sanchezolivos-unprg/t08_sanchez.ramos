7e26684fb6d472ff6be64e955863900561ebd664
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-13 03:25:09 -0500

    se agrego la concatenacion 1 en el python file 4_concatenacion.py

---
+++ sanchez/4_concatenacion.py
@@ -0,0 +1,8 @@
+# CONCATENACION 1:
+animal="leon"
+mc_kg=130
+prom_vida=14
+
+# agrupacion de cadenas
+mensaje1=("el " + animal + " es el rey de la selva, su peso es de " + str(mc_kg) + "kg y su promedio de vida es de " + str(prom_vida) + " años")
+print(mensaje1)
