e6260ffcd28fa1d719f4af912a9f195df8f59b0f
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-13 03:33:23 -0500

    se agrego el cortado de cadenas 2 en el python file 5_cortado.py

--- sanchez/5_cortado.py
+++ sanchez/5_cortado.py
@@ -11,3 +11,12 @@ print(msg1[39:35:-1])
 
 # mostrar PEDRO
 print(msg1[24:29])
+
+# CORTADO DE CADENAS 2:
+#     0         1         2         3         4
+#     012345678901234567890123456789012345678901234567
+msg2="SI NO QUIERES SER DERROTADO, SOLO SIGUE ADELANTE"
+
+# Mostrar la cadena: DERROTADO No
+subcadena=msg2[18:27] + " " + msg2[3:5]
+print(subcadena)
