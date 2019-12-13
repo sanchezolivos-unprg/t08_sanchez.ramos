2e768dc4b35626133a38e8be2e0324c0446bf282
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-13 02:58:04 -0500

    se agrego la indexacion 2 en python file 1_indexacion

--- sanchez/1_indexacion.py
+++ sanchez/1_indexacion.py
@@ -7,3 +7,11 @@ esc="ESCUELA DE INGENIERIA ELECTRONICA"
 # pais= esc[18] + esc[3] + esc[1] + esc[19] + esc[20]
 print(esc[18], esc[3], esc[1], esc[19], esc[20])
 
+# INDEXACION 2:
+# un poema
+#    0         1         2         3         4         5         6
+#    0123456789012345678901234567890123456789012345678901234567890123456
+pma="EN LA PUERTA DE MI CASA PLANTE DOS ARBOLES UN TE QUIERO Y UN TE AMO"
+
+# nombres= patricia, elton
+print(pma[6] + " " + pma[20] + " " + pma[10] + " " + pma[36] + " " + pma[51] + " " + pma[19] + " " + pma[17] + " " + pma[35] + ", " + " " + pma[8] + " " + pma[25] + " " + pma[46] + " " + pma[66] + " " + pma[59])
