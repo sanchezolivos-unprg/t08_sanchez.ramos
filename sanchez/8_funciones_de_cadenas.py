4ecd7724e7111c1aae82f52de85b6a10aaea6bdf
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-13 03:48:46 -0500

    se agrego funciones de manipulacion de cadena 2 en el python file 8_funciones_de_cadena.py

--- sanchez/8_funciones_de_cadenas.py
+++ sanchez/8_funciones_de_cadenas.py
@@ -4,3 +4,10 @@ msg="EL FIN DEL MUNDO SE ACERCA"
 # Mostrar el nro de ocurrencias de la palabra FIN
 print("FIN",msg.count("FIN"))
 print("A ->", msg.count("A"))
+
+# FUNCIONES DE MANIPULACION DE CADENA 2:
+cadena1="LOS VENGADORES FIN DE LA GUERRA"
+
+# Transformar el texto en minusculas
+mensaje=cadena1.lower()
+print(mensaje)
