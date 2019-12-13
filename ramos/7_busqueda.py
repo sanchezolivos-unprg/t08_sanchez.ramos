1df839a6c92d23f7cc6ab2c083ec5124ae70ad84
sanchezolivos-unprg <57330967+sanchezolivos-unprg@users.noreply.github.com>
2019-12-13 04:24:29 -0500

    se agrego la continuacion de la operacion busqueda de cadenas desde 16 hasta el ejercicio 20 en el directorio ramos

---
+++ ramos/7_busqueda.py
@@ -0,0 +1,22 @@
+#OPERACION BUSQUEDA DE CADENAS
+
+#manipulador de texto nro 16
+#       0123456789
+cadena="hola mundo"
+print(cadena.index("n"))  # me mostrara el indice que ocupa el caracter ->n
+
+#manipulador de texto nro 17
+cad="QUIEN QUIERE VIAJAR CONMIGO"
+print(cad.find("QUIERE"))
+
+#manipulador de texto nro 18
+cad1="EL MUNDO ES UN LIBRO"
+print(cad1.index("LIBRO"))
+
+#manipulador de texto nro 19
+cad1="EL MUNDO ES UN LIBRO"
+print(cad1.index("EL"))
+
+#manipulador de texto nro 20
+cad1="EL MUNDO ES UN LIBRO"
+print(cad1.find("MUNDO"))
